7066cde3ee2ce27c8655a306a6463625b6d6eb98
Amber Frantz <amber.k.frantz@mgail.com>
2022-09-09 20:54:54 -0400

    Complted Assignment

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -13,6 +13,10 @@ class MembersOnlyArticlesController < ApplicationController
 
   private
 
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
